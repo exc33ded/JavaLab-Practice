26a6329fc48e5763004aa912e2ef62c17a6866bf
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2024-01-30 10:42:25 +0530

    Create TestOddAndEven.java

---
+++ Week5/TestOddAndEven.java
@@ -0,0 +1,40 @@
+// Week 5 Question 1
+
+package Week5;
+import java.util.Scanner;
+
+class OddAndEven {
+    private int countOfOdd;
+    private int countOfEven;
+
+    public void addNumber(int number) {
+        if (number % 2 == 0) {
+            countOfEven++;
+        } else {
+            countOfOdd++;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Number of Odd: " + countOfOdd + ", Number of Even: " + countOfEven;
+    }
+}
+
+public class TestOddAndEven {
+    public static void main(String[] args) {
+        OddAndEven oddAndEven = new OddAndEven();
+        Scanner sc = new Scanner(System.in);
+        String input;
+
+        System.out.println("Enter numbers (Q to quit):");
+        
+        while (!(input = sc.nextLine()).equalsIgnoreCase("Q")) {
+            int number = Integer.parseInt(input);
+            oddAndEven.addNumber(number);
+        }
+
+        System.out.println(oddAndEven);
+    }
+}
+
