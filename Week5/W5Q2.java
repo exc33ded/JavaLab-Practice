b84a93924d80975615beb21720edde3826fb08ab
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2024-01-30 10:47:50 +0530

    Create W5Q2.java

---
+++ Week5/W5Q2.java
@@ -0,0 +1,40 @@
+package Week5;
+import java.util.Scanner;
+
+class Circle{
+    private final double radius;
+    private final double x_center;
+    private final double y_center;
+    
+    Circle(double Radius, double X_center, double Y_center){
+        radius = Radius;
+        x_center = X_center;
+        y_center = Y_center;
+    }
+    public double Area(double radius){
+        return Math.PI * Math.pow(radius, 2);
+    }
+    
+    public double Perimeter(double radius){
+        return 2 * Math.PI * radius;
+    }
+    
+    public boolean isPointInside(double x, double y) {
+        double distance = Math.sqrt(Math.pow((x_center - x), 2) + Math.pow((y_center - y), 2));
+        return distance <= radius;
+    }
+}
+
+public class W5Q2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double radius, x_center, y_center;
+        System.out.print("Enter radius for the circle: ");
+        radius = sc.nextDouble();
+        System.out.print("Enter center X Coordinate for the circle: ");
+        x_center = sc.nextDouble();
+        System.out.print("Enter Y Coordinate for the circle: ");
+        y_center = sc.nextDouble();
+        Circle cir = new Circle(radius, x_center, y_center);
+    }
+}
