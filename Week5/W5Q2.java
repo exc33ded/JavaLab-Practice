a8da7ed7ef3727fdcb99ca04189a07100bd73c0b
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2024-02-01 15:04:30 +0530

    Update W5Q2.java

--- Week5/W5Q2.java
+++ Week5/W5Q2.java
@@ -29,12 +29,34 @@ public class W5Q2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double radius, x_center, y_center;
-        System.out.print("Enter radius for the circle: ");
-        radius = sc.nextDouble();
+        
         System.out.print("Enter center X Coordinate for the circle: ");
         x_center = sc.nextDouble();
         System.out.print("Enter Y Coordinate for the circle: ");
         y_center = sc.nextDouble();
-        Circle cir = new Circle(radius, x_center, y_center);
+            
+        while (true) {
+            System.out.print("Enter radius for the circle: ");
+            radius = sc.nextDouble();
+            
+            Circle cir = new Circle(radius, x_center, y_center);
+            
+            if (radius < 0) {
+                System.out.println("Radius is negative.");
+                break;
+            }
+            else{
+                System.out.println("---------------------------------------------");
+                System.out.println("Area: " + cir.Area(radius));
+                System.out.println("Perimeter: " + cir.Perimeter(radius));
+                if (cir.isPointInside(x_center, y_center)){
+                    System.out.println("Points lie inside the circle");
+                }
+                else{
+                    System.out.println("Points lie outside the circle");
+                }
+            }
+        }
     }
 }
+
