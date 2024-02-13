56250341f3861da77bbbbb0f75f86f9d9b24859c
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2024-02-13 09:54:48 +0530

    Update W3Q5ii.java

--- Week3/W3Q5ii.java
+++ Week3/W3Q5ii.java
@@ -1,21 +1,29 @@
 package Week3;
 import java.util.Scanner;
 
-public class W3Q5ii {
+public class W3Q5 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the number of rows: ");
-        int rows = scanner.nextInt();
-
-        for (int i = 1; i <= rows; i++) {
-            System.out.print("");
-
-            for (int j = 1; j <= i; j++) {
-                System.out.print(j % 2 == 0 ? "0" : "1");
+        Scanner sc = new Scanner(System.in);
+        int num = 1;
+        System.out.print("Enter then number of rows to produce: ");
+        int n = sc.nextInt();
+        System.out.println("\nPart 1\n");
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print(num + " ");
+                num++;
             }
-
             System.out.println();
         }
+        
+    System.out.println("\nPart 2\n");
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= i; j++) {
+            System.out.print((i + j) % 2 + " ");
+        }
+        System.out.println();
+    }
     }
 }
 
