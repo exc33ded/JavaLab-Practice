f23c18fcf898594afb210158d186d3460e4d49de
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2024-02-13 09:55:05 +0530

    Update W4Q2.java

--- Week4/W4Q2.java
+++ Week4/W4Q2.java
@@ -5,16 +5,22 @@ public class W4Q2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter three digits (0-9) with space between them: ");
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
+        int[] numbers = new int[3];
+        for (int i = 0; i < 3; i++) {
+            numbers[i] = sc.nextInt();
+        }
         System.out.println("All possible combinations are:");
-        System.out.println(a + "" + b + "" + c);
-        System.out.println(a + "" + c + "" + b);
-        System.out.println(b + "" + a + "" + c);
-        System.out.println(b + "" + c + "" + a);
-        System.out.println(c + "" + a + "" + b);
-        System.out.println(c + "" + b + "" + a);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (j != i) {
+                    for (int k = 0; k < 3; k++) {
+                        if (k != i && k != j) {
+                            System.out.println(numbers[i] + "" + numbers[j] + "" + numbers[k]);
+                        }
+                    }
+                }
+            }
+        }
         sc.close();
     }
 }
