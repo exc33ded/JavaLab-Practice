26fd14214c1ffb7e3b9689bec64fcecf6e74c6e4
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2024-02-13 09:55:49 +0530

    Update W4Q3.java

--- Week4/W4Q3.java
+++ Week4/W4Q3.java
@@ -5,12 +5,13 @@ public class W4Q3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter 4 numbers: ");
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
-        int num3 = sc.nextInt();
-        int num4 = sc.nextInt();
-        int sum = num1 + num2 + num3 + num4;
-        int product = num1 * num2 * num3 * num4;
+        int sum = 0;
+        int product = 1;
+        for (int i = 0; i < 4; i++) {
+            int num = sc.nextInt();
+            sum += num;
+            product *= num;
+        }
         System.out.println("The sum of the numbers is " + sum + ".");
         System.out.println("The product of the numbers is " + product + ".");
         sc.close();
